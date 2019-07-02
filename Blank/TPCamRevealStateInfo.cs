533d11077a454e181f1c307fd1f2acf4c1208524
SquareUnit <38660584+SquareUnit@users.noreply.github.com>
2019-07-01 20:58:05 -0400

    Update and rename TPCamRevealStateInfo to TPCamRevealStateInfo.cs

--- Blank/TPCamRevealStateInfo.cs
+++ Blank/TPCamRevealStateInfo.cs
@@ -1,3 +1,11 @@
+/// Designed by Félix Desrosiers-Dorval
+/// Last modification date : 2019-07-01
+/// Last feature added : 
+/// https://github.com/SquareUnit/Code-Storage
+
+/// Simplistic tool made for the level designer so they could store data, telling what the camera reveal would do
+/// during it's state time. Very simple. Allow rotations and panning, cannot be done in sequences. Thats to be changed.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
